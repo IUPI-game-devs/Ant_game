9d0232411d0953055b76768bfac0c35a894a3415
edward.garcia5 <edward.garcia5@upr.edu>
2023-12-11 23:22:50 -0400

    chage of camera, player follows mouse
    had to add another package called cinemachine so that it could transition from tunel to topdown.

--- Assets/Scipts/Player.cs
+++ Assets/Scipts/Player.cs
@@ -6,26 +6,22 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     public float speed;
-    public float rotationSpeed;
     private Vector2 movementValue;
     private float lookValue;
 
-    private void Awake(){
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-    }
+    [SerializeField] private Camera cam;
 
     public void OnMove(InputValue value){
         movementValue = value.Get<Vector2>() * speed;
     }
 
-    public void OnLook(InputValue value){
-        lookValue = value.Get<Vector2>().x * rotationSpeed;
-    }
     // Update is called once per frame
     void Update()
-    {
+    {   
+        Vector3 mousePos = Mouse.current.position.ReadValue();
+        mousePos.z = 100f;
+        mousePos = cam.ScreenToWorldPoint(mousePos) - transform.position;
         transform.Translate(movementValue.x * Time.deltaTime, 0, movementValue.y * Time.deltaTime);
-        transform.Rotate(0, lookValue * Time.deltaTime, 0);
+        transform.rotation = Quaternion.LookRotation(mousePos, Vector3.up); 
     }
 }
