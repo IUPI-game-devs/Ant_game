57509c973117076219f33b217ca608e4b80e2346
Luis <luisfernandojavier.velazquez@upr.edu>
2023-12-13 00:31:20 -0400

    Add code for enemies

--- Assets/Scripts/Life.cs
+++ Assets/Scripts/Life.cs
@@ -1,25 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Events;
 
 public class Life : MonoBehaviour
 {
     public int maxHealth = 100;
     public int currentHealth;
     public HealthBar healthBar;
+
+    public UnityEvent onDeath; 
 // Start is called before the first frame update
     void Start()
     {
     currentHealth = maxHealth;
     healthBar.SetMaxHealth(maxHealth);
-    
+
     }
 // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space)){
             TakeDamage(20);
+        }
+        if ( currentHealth <= 0 )
+        {
+            onDeath.Invoke();
+            Destroy(gameObject);
         }
     }
 
